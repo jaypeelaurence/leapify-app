0a00c109c5a8aa7f6e9387e5e904f4c24adcdd1d
jaypeelaurence <jaypeelaurencecocjin@gmail.com>
2022-10-27 19:58:45 +0800

    feat: updated the button component

---
+++ src/ui/components/Button/Button.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import cn from 'classnames';
+import PropTypes from 'prop-types';
+
+const Button = ({ label, icon, iconLeft, iconRight, className, ...props }) => {
+  return (
+    <button
+      type="button"
+      className={cn(
+        'btn',
+        className
+      )}
+      {...props}
+    >
+      <div>
+        { !iconLeft ? null : icon() }
+        {label}
+        { !iconRight ? null : icon() }
+      </div>
+    </button>
+  );
+};
+
+export default Button;
+
+Button.propTypes = {
+  type: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  className: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.sting)
+  ]),
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  iconLeft: PropTypes.bool,
+  iconRight: PropTypes.bool,
+  icon: PropTypes.instanceOf(Function)
+};
+
+Button.defaultProps = {
+  onClick: () => null,
+  type: 'button',
+  className: null,
+  disabled: false,
+  iconLeft: false,
+  iconRight: false,
+  icon: () => null 
+};
---
+++ src/ui/components/Button/Button.stories.js
@@ -0,0 +1,122 @@
+import React from 'react';
+
+import cn from 'classnames';
+import { BsPlus } from "react-icons/bs";
+
+import Button from './Button';
+import 'assets/styles/styles.css';
+
+export default {
+  title: 'Components/General/Button',
+  component: Button,
+  argTypes: {
+    label: {
+      defaultValue: 'BUTTON',
+      description: 'Label of the Button'
+    },
+    disabled: {
+      defaultValue: false,
+      description: 'Disable toggle of the Button'
+    },
+    size: {
+      title: 'Size',
+      defaultValue: 'Medium',
+      description: 'Size of the Button',
+      control: 'radio',
+      options: ['Large', 'Medium', 'Small'],
+      mapping: {
+        Large: 'lg',
+        Medium: 'md',
+        Small: 'sm'
+      }
+    },
+    hasIcon: {
+      title: 'Has Icon',
+      defaultValue: false,
+      description: 'Button has Icon',
+      control: 'boolean'
+    },
+    iconDirection: {
+      title: 'Icon Direction',
+      defaultValue: 'Left',
+      description: 'Direction of Icon in the Button',
+      options: ['Left', 'Right'],
+      control: { type: 'radio' },
+    },
+    onClick: {
+      action: 'clicked',
+    }
+  }
+};
+
+const Template = ({ type, size, hasIcon, iconDirection, ...args }) => {
+  const props = {
+    icon: BsPlus,
+    iconLeft: iconDirection == 'Left' ? hasIcon : false,
+    iconRight: iconDirection == 'Right' ? hasIcon : false,
+  };
+
+  return (
+    <Button
+      className={cn(
+        type,
+        size
+      )}
+      {...props}
+      {...args}
+    />
+  );
+};
+
+const ARGS = {
+  label: "BUTTON"
+};
+
+const PARAMS = {
+  controls: {
+    exclude: [
+      'className',
+      'iconRight',
+      'iconLeft',
+      'type',
+      'variant',
+      'icon'
+    ]
+  }
+};
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  ...ARGS,
+  type: "primary"
+};
+
+Primary.parameters = PARAMS;
+
+export const Secondary = Template.bind({});
+
+Secondary.args = {
+  ...ARGS,
+  type: "secondary"
+};
+
+Secondary.parameters = PARAMS;
+
+export const Tertiary = Template.bind({});
+
+Tertiary.args = {
+  ...ARGS,
+  type: "tertiary"
+};
+
+Tertiary.parameters = PARAMS;
+
+export const Text = Template.bind({});
+
+Text.args = {
+  ...ARGS,
+  type: null
+};
+
+Text.parameters = PARAMS;
