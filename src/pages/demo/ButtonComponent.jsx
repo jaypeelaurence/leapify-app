68009a4a6f14490415c4134af4906531358cc9fc
jaypeelaurence <jaypeelaurencecocjin@gmail.com>
2022-10-28 13:34:28 +0800

    feat: simplified the button component

--- src/pages/demo/ButtonComponent.jsx
+++ src/pages/demo/ButtonComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Button from 'ui/components';
+import { Button } from 'ui/components';
 
 function ButtonComponent() {
   return (
--- src/ui/components/Button/Button.stories.jsx
+++ src/ui/components/Button/Button.stories.jsx
@@ -1,7 +1,10 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
 import React from 'react';
 
 import cn from 'classnames';
-import { BsPlus } from "react-icons/bs";
+import { BsPlus } from 'react-icons/bs';
+import PropTypes from 'prop-types';
 
 import Button from './Button';
 import 'assets/styles/styles.css';
@@ -12,11 +15,11 @@ export default {
   argTypes: {
     label: {
       defaultValue: 'BUTTON',
-      description: 'Label of the Button'
+      description: 'Label of the Button',
     },
     disabled: {
       defaultValue: false,
-      description: 'Disable toggle of the Button'
+      description: 'Disable toggle of the Button',
     },
     size: {
       title: 'Size',
@@ -27,14 +30,14 @@ export default {
       mapping: {
         Large: 'lg',
         Medium: 'md',
-        Small: 'sm'
-      }
+        Small: 'sm',
+      },
     },
     hasIcon: {
       title: 'Has Icon',
       defaultValue: false,
       description: 'Button has Icon',
-      control: 'boolean'
+      control: 'boolean',
     },
     iconDirection: {
       title: 'Icon Direction',
@@ -45,31 +48,51 @@ export default {
     },
     onClick: {
       action: 'clicked',
-    }
-  }
+    },
+  },
 };
 
-const Template = ({ type, size, hasIcon, iconDirection, ...args }) => {
+function Template({
+  type,
+  size,
+  hasIcon,
+  iconDirection,
+  ...args
+}) {
   const props = {
     icon: BsPlus,
-    iconLeft: iconDirection == 'Left' ? hasIcon : false,
-    iconRight: iconDirection == 'Right' ? hasIcon : false,
+    iconLeft: iconDirection === 'Left' ? hasIcon : false,
+    iconRight: iconDirection === 'Right' ? hasIcon : false,
   };
 
   return (
     <Button
       className={cn(
         type,
-        size
+        size,
       )}
       {...props}
       {...args}
     />
   );
+}
+
+Template.propTypes = {
+  type: PropTypes.string,
+  size: PropTypes.bool,
+  hasIcon: PropTypes.bool,
+  iconDirection: PropTypes.string,
+};
+
+Template.defaultProps = {
+  type: 'primary',
+  size: 'md',
+  hasIcon: true,
+  iconDirection: 'Left',
 };
 
 const ARGS = {
-  label: "BUTTON"
+  label: 'BUTTON',
 };
 
 const PARAMS = {
@@ -80,16 +103,16 @@ const PARAMS = {
       'iconLeft',
       'type',
       'variant',
-      'icon'
-    ]
-  }
+      'icon',
+    ],
+  },
 };
 
 export const Primary = Template.bind({});
 
 Primary.args = {
   ...ARGS,
-  type: "primary"
+  type: 'primary',
 };
 
 Primary.parameters = PARAMS;
@@ -98,7 +121,7 @@ export const Secondary = Template.bind({});
 
 Secondary.args = {
   ...ARGS,
-  type: "secondary"
+  type: 'secondary',
 };
 
 Secondary.parameters = PARAMS;
@@ -107,7 +130,7 @@ export const Tertiary = Template.bind({});
 
 Tertiary.args = {
   ...ARGS,
-  type: "tertiary"
+  type: 'tertiary',
 };
 
 Tertiary.parameters = PARAMS;
@@ -116,7 +139,16 @@ export const Text = Template.bind({});
 
 Text.args = {
   ...ARGS,
-  type: null
+  type: null,
 };
 
 Text.parameters = PARAMS;
+
+export const Link = Template.bind({});
+
+Link.args = {
+  ...ARGS,
+  type: 'link',
+};
+
+Link.parameters = PARAMS;
